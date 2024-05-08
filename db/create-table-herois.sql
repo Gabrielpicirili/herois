275b275bb03f0bdb812e610b7f41bfc762e0b997
Gabrielpicirili <gabriel.picirili@aluno.senai.br>
2024-05-08 14:52:30 -0300

    feat inicio projeto

---
+++ db/create-table-herois.sql
@@ -0,0 +1,21 @@
+CREATE TABLE heros(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    poder_values INTERGER NOT NULL,
+    poder_type VARCHAR(255) NOT NULL,
+    hp INTERGER NOT NULL,
+    attack INTERGER NOT NULL
+);
+
+INSERT INTO heros (name, poder_values, poder_type, hp, attack) VALUES ('Superman'   50, 'kriptoniano',  3000,  280 );
+INSERT INTO heros (name, poder_values, poder_type, hp, attack) VALUES ('Batman'   35, 'Rico',  2200,  180 );
+INSERT INTO heros (name, poder_values, poder_type, hp, attack) VALUES ('Spider-man'   40, 'homem aranha',  2500,  200 );
+INSERT INTO heros (name, poder_values, poder_type, hp, attack) VALUES ('Wonder Woman '   45, 'guerraira',  2800,  270 );
+INSERT INTO heros (name, poder_values, poder_type, hp, attack) VALUES ('Iron Man'   48, 'inteligente',  2800,  250 );
+INSERT INTO heros (name, poder_values, poder_type, hp, attack) VALUES ('Captain America '   42, 'forte',  2600,  220 );
+INSERT INTO heros (name, poder_values, poder_type, hp, attack) VALUES ('Captain America '   42, 'forte',  2600,  220 );
+INSERT INTO heros (name, poder_values, poder_type, hp, attack) VALUES ('Captain America '   42, 'forte',  2600,  220 );
+INSERT INTO heros (name, poder_values, poder_type, hp, attack) VALUES ('Captain America '   42, 'forte',  2600,  220 );
+INSERT INTO heros (name, poder_values, poder_type, hp, attack) VALUES ('Captain America '   42, 'forte',  2600,  220 );
+INSERT INTO heros (name, poder_values, poder_type, hp, attack) VALUES ('Captain America '   42, 'forte',  2600,  220 );
+INSERT INTO heros (name, poder_values, poder_type, hp, attack) VALUES ('Captain America '   42, 'forte',  2600,  220 );
