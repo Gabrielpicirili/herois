d9e3ab8553102a774fc26fc98b81d28553df34eb
Gabrielpicirili <gabriel.picirili@aluno.senai.br>
2024-05-08 16:34:28 -0300

    feat meio

--- db/create-table-herois.sql
+++ db/create-table-herois.sql
@@ -1,18 +1,18 @@
 CREATE TABLE heroes(
     id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
-    power_values INTERGER NOT NULL,
+    power_values INTEGER NOT NULL,
     power_type VARCHAR(255) NOT NULL,
-    hp INTERGER NOT NULL,
-    attack INTERGER NOT NULL
+    hp INTEGER NOT NULL,
+    attack INTEGER NOT NULL
 );
 
-INSERT INTO heroes (name, power_values, power_type, hp, attack) VALUES ('Superman'   50, 'kriptoniano',  3000,  280 );
-INSERT INTO heroes (name, power_values, power_type, hp, attack) VALUES ('Batman'   35, 'Rico',  2200,  180 );
-INSERT INTO heroes (name, power_values, power_type, hp, attack) VALUES ('Spider-man'   40, 'homem aranha',  2500,  200 );
-INSERT INTO heroes (name, power_values, power_type, hp, attack) VALUES ('Wonder Woman '   45, 'guerraira',  2800,  270 );
-INSERT INTO heroes (name, power_values, power_type, hp, attack) VALUES ('Iron Man'   48, 'inteligente',  2800,  250 );
-INSERT INTO heroes (name, power_values, power_type, hp, attack) VALUES ('Captain America '   42, 'forte',  2600,  220 );
+INSERT INTO heroes (name, power_values, power_type, hp, attack) VALUES ('Superman', 50, 'kriptoniano', 3000, 280);
+INSERT INTO heroes (name, power_values, power_type, hp, attack) VALUES ('Batman', 35, 'Rico', 2200, 180);
+INSERT INTO heroes (name, power_values, power_type, hp, attack) VALUES ('Spider-man', 40, 'homem aranha', 2500, 200);
+INSERT INTO heroes (name, power_values, power_type, hp, attack) VALUES ('Wonder Woman', 45, 'guerreira', 2800, 270);
+INSERT INTO heroes (name, power_values, power_type, hp, attack) VALUES ('Iron Man', 48, 'inteligente', 2800, 250);
+INSERT INTO heroes (name, power_values, power_type, hp, attack) VALUES ('Captain America', 42, 'forte', 2600, 220);
 
 CREATE TABLE batalhas (
     id INTEGER PRIMARY KEY,
