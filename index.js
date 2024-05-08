8bd6d3509070091ad8d18920c96a915a49a11f27
Gabrielpicirili <gabriel.picirili@aluno.senai.br>
2024-05-08 14:56:21 -0300

    feat commit

--- index.js
+++ index.js
@@ -17,3 +17,29 @@ app.use(express.json());
 app.listen(PORT, () => {
   console.log(`funcionando normalmente ${PORT}🚀`);
 });
+
+app.get("/heros", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT * FROM heros");
+    res.json(result.rows);
+  } catch (err) {
+    console.log(err);
+  }
+});
+app.get("/", async (req, res) => {
+    try {
+        res.json({ message: "Bem-Vindo a batalha de herois." });
+    } catch (err) {
+        console.log(err);
+    }
+    });
+    
+app.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const result = await pool.query(`SELECT * FROM heros WHERE id =$1`, [id]);
+    res.json(result.rows);
+  } catch (err) {
+    console.log(err);
+  }
+});
