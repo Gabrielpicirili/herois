00743d6d7d8d2d1c988c74539fc19197ba49dfc6
Gabrielpicirili <gabriel.picirili@aluno.senai.br>
2024-05-08 15:40:57 -0300

    feat commit

--- db/create-table-herois.sql
+++ db/create-table-herois.sql
@@ -1,28 +1,28 @@
-CREATE TABLE heros(
+CREATE TABLE heroes(
     id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
-    poder_values INTERGER NOT NULL,
-    poder_type VARCHAR(255) NOT NULL,
+    power_values INTERGER NOT NULL,
+    power_type VARCHAR(255) NOT NULL,
     hp INTERGER NOT NULL,
     attack INTERGER NOT NULL
 );
 
-INSERT INTO heros (name, poder_values, poder_type, hp, attack) VALUES ('Superman'   50, 'kriptoniano',  3000,  280 );
-INSERT INTO heros (name, poder_values, poder_type, hp, attack) VALUES ('Batman'   35, 'Rico',  2200,  180 );
-INSERT INTO heros (name, poder_values, poder_type, hp, attack) VALUES ('Spider-man'   40, 'homem aranha',  2500,  200 );
-INSERT INTO heros (name, poder_values, poder_type, hp, attack) VALUES ('Wonder Woman '   45, 'guerraira',  2800,  270 );
-INSERT INTO heros (name, poder_values, poder_type, hp, attack) VALUES ('Iron Man'   48, 'inteligente',  2800,  250 );
-INSERT INTO heros (name, poder_values, poder_type, hp, attack) VALUES ('Captain America '   42, 'forte',  2600,  220 );
+INSERT INTO heroes (name, power_values, power_type, hp, attack) VALUES ('Superman'   50, 'kriptoniano',  3000,  280 );
+INSERT INTO heroes (name, power_values, power_type, hp, attack) VALUES ('Batman'   35, 'Rico',  2200,  180 );
+INSERT INTO heroes (name, power_values, power_type, hp, attack) VALUES ('Spider-man'   40, 'homem aranha',  2500,  200 );
+INSERT INTO heroes (name, power_values, power_type, hp, attack) VALUES ('Wonder Woman '   45, 'guerraira',  2800,  270 );
+INSERT INTO heroes (name, power_values, power_type, hp, attack) VALUES ('Iron Man'   48, 'inteligente',  2800,  250 );
+INSERT INTO heroes (name, power_values, power_type, hp, attack) VALUES ('Captain America '   42, 'forte',  2600,  220 );
 
 CREATE TABLE batalhas (
     id INTEGER PRIMARY KEY,
-    heros_p INTEGER NOT NULL,
-   heros_s INTEGER NOT NULL,
-    FOREIGN KEY (heros_p) REFERENCES heros(id),
-    FOREIGN KEY (heros_s) REFERENCES heros(id)
+    heroes_p INTEGER NOT NULL,
+   heroes_s INTEGER NOT NULL,
+    FOREIGN KEY (heroes_p) REFERENCES heroes(id),
+    FOREIGN KEY (heroes_s) REFERENCES heroes(id)
 );
 
-INSERT INTO batalhas (id,heros_p, heros_s) VALUES (1,1, 2);
+INSERT INTO batalhas (id,heroes_p, heroes_s) VALUES (1,1, 2);
 
 CREATE OR REPLACE FUNCTION obter_vencedor_batalha(batalha_id INTEGER)
 RETURNS VARCHAR(255) AS
@@ -37,8 +37,8 @@ BEGIN
                ELSE 'Empate'
            END INTO winner_name
     FROM batalhas AS b
-    INNER JOIN heros AS h1 ON b.heros_p = h1.id
-    INNER JOIN heros AS h2 ON b.heros_s = h2.id
+    INNER JOIN heroes AS h1 ON b.heroes_p = h1.id
+    INNER JOIN heroes AS h2 ON b.heroes_s = h2.id
     WHERE b.id = batalha_id;
 
     RETURN winner_name;
