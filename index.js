16fa6ecce0080a33dc8cb26f4a4ec323c564f7ad
Gabrielpicirili <gabriel.picirili@aluno.senai.br>
2024-05-08 14:57:45 -0300

    feat  commit

--- index.js
+++ index.js
@@ -43,3 +43,29 @@ app.get("/:id", async (req, res) => {
     console.log(err);
   }
 });
+app.post("/heros", async (req, res) => {
+  const { name, power_values, power_type, hp, attack } = req.body;
+  try {
+   await pool.query(
+      `INSERT INTO heros (name, power_values, power_type, hp , attack ) VALUES ($1, $2,$3,$4,$5)`,
+      [name, power_values, power_type, hp, attack]
+    );
+    res.json({ message: "Heroi cadastrado com sucesso" });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+app.put("/:id", async (req, res) => {
+  const { id } = req.params;
+  const { name, power_values, power_type, hp, attack } = req.body;
+  try {
+    const result = await pool.query(
+      `UPDATE heros SET name = $1, power_values = $2, power_type = $3, hp = $4, attack = $5 WHERE id = $6`,
+      [name, power_values, power_type, hp, attack, id]
+    );
+    res.json(result.rows);
+  } catch (err) {
+    console.log(err);
+  }
+});
